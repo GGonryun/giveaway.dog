2f51788c6dc7916e7e67c269408586bb5f08fb75
Miguel Campos <amodestduck@gmail.com>
2025-07-29 00:39:15 -0700

    add array context helper

---
+++ components/hooks/use-array-context.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export type ArrayContextValue = {
+  index: number;
+};
+
+export const ArrayContext = React.createContext<ArrayContextValue>(
+  {} as ArrayContextValue
+);
+
+export const useArrayContext = () => {
+  const context = React.useContext(ArrayContext);
+  if (!context) {
+    throw new Error(
+      'useArrayContext must be used within a ArrayContext.Provider'
+    );
+  }
+  return context;
+};
