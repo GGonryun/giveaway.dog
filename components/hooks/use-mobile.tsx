826fe7a45a3b7d09c615781fdc474fcc9feb2e64
Miguel Campos <amodestduck@gmail.com>
2025-08-07 02:10:38 -0700

    mobile hook checks tablets too

--- components/hooks/use-mobile.tsx
+++ components/hooks/use-mobile.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 // https://tailwindcss.com/docs/responsive-design
-// --breakpoint-sm
-const MOBILE_BREAKPOINT = 640; // px
+// --breakpoint-md
+const MOBILE_BREAKPOINT = 768; // px
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
