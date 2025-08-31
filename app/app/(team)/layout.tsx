9c5b8eb33cfdefe51b94a949fb4c474ecd0efd3d
Miguel Campos <amodestduck@gmail.com>
2025-08-31 09:25:30 -0700

    fix invalid params

--- app/app/(team)/layout.tsx
+++ app/app/(team)/layout.tsx
@@ -1,23 +1,15 @@
 import getUser from '@/procedures/user/get-user';
-import getUserTeam from '@/procedures/teams/get-user-team';
-import getUserTeams from '@/procedures/teams/get-user-teams';
-import { TeamsProvider } from '@/components/context/team-provider';
 import { UserProvider } from '@/components/context/user-provider';
 import { redirect } from 'next/navigation';
 
 export default async function Layout({
-  children,
-  params
+  children
 }: {
   children: React.ReactNode;
-  params: Promise<{ slug: string }>;
 }) {
-  const resolvedParams = await params;
   const user = await getUser();
   if (!user.ok) {
-    console.error(
-      `Failed to get user context for slug: ${resolvedParams.slug}`
-    );
+    console.error(`Failed to get user context`);
     redirect(`/app`);
   }
 
